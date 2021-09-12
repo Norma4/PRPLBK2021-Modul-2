2afb56df3846769428c331129ea4bf81ec15f67b
Muhamad Irham Maulana <irhammaulana1077@gmail.com>
2021-09-12 20:39:58 +0700

    update index.js

--- tugas_modul2_kel42/src/index.js
+++ tugas_modul2_kel42/src/index.js
@@ -3,8 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 import NavBar from "./components/NavBar";
+import Table from "./components/Table";
 
 import User from "./assets/images/user.png";
+import Man from "./assets/images/man.png";
+import Woman from "./assets/images/woman.png";
 
 const navigation = [
   { name: 'Dashbor', href: '#', current: false },
@@ -13,6 +16,20 @@ const navigation = [
   { name: 'Jadwal', href: '#', current: false },
 ];
 
+const people = [
+  {
+    name: 'Muhamad Irham Maulana',
+    group: '42',
+    email: 'irham@mail.com',
+    image: Man,
+  },
+  {
+    name: 'Norma Sakinah',
+    group: '42',
+    email: 'norma@mail.com',
+    image: Woman,
+  },
+];
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,6 +37,11 @@ ReactDOM.render(
       navigation = {navigation}
       title = "Praktikum RPLBK 2021"
       user = {User}
+    />
+    <Table
+      people = {people}
+      department = "Teknik Komputer"
+      college = "Universitas Diponegoro"
     />
   </React.StrictMode>,
   document.getElementById('root')
