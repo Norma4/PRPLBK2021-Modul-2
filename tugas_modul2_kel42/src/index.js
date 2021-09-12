105870b250ead6a89f175015881f60c920be4675
Muhamad Irham Maulana <irhammaulana1077@gmail.com>
2021-09-12 20:23:49 +0700

    update index.js

--- tugas_modul2_kel42/src/index.js
+++ tugas_modul2_kel42/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+import NavBar from "./components/NavBar";
+
+import User from "./assets/images/user.png";
+
+const navigation = [
+  { name: 'Dashbor', href: '#', current: false },
+  { name: 'Praktikan', href: '#', current: true },
+  { name: 'Asisten', href: '#', current: false },
+  { name: 'Jadwal', href: '#', current: false },
+];
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NavBar
+      navigation = {navigation}
+      title = "Praktikum RPLBK 2021"
+      user = {User}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
